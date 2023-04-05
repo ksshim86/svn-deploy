dba3fb9b5e3b20f15cd5ce7fc0941830a375cc85
ksshim86 <ksshim86@gmail.com>
2023-04-05 17:23:07 +0900

    # 상수 클래스 추가

---
+++ src/main/java/com/ks/sd/consts/SdConstants.java
@@ -0,0 +1,12 @@
+package com.ks.sd.consts;
+
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+public final class SdConstants {
+    public final static String UNDELETED = "N"; // 삭제되지 않은 데이터
+    public final static String INACTIVE = "N"; // 비활성화
+    public final static String STARTED = "Y"; // 시작
+    public final static String STOPPED = "N"; // 중지
+    public final static String COMPLETION = "N"; // 완료
+}
