a47ede552f976036c800efb6e727619b4c423992
ksshim86 <ksshim86@gmail.com>
2023-04-06 13:18:26 +0900

    # 프로젝트 검색 기능 추가

--- src/main/java/com/ks/sd/api/pjt/repository/ProjectRepository.java
+++ src/main/java/com/ks/sd/api/pjt/repository/ProjectRepository.java
@@ -4,10 +4,11 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import com.ks.sd.api.pjt.entity.Project;
 
-public interface ProjectRepository extends JpaRepository<Project, Integer> {
+public interface ProjectRepository extends JpaRepository<Project, Integer>, JpaSpecificationExecutor<Project> {
     Optional<List<Project>> findByDelYn(String delYn);
 
     Optional<List<Project>> findByDelYnAndDpStAndStartedYnAndRcsSt(String delYn, String dpSt, String startedYn, String rcsSt);
