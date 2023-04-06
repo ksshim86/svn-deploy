9a1f4669cd9127b95f0ec9b2ebf4dcfb52f03ed8
ksshim86 <ksshim86@gmail.com>
2023-04-06 13:18:44 +0900

    # 상수 추가

--- src/main/java/com/ks/sd/consts/SdConstants.java
+++ src/main/java/com/ks/sd/consts/SdConstants.java
@@ -4,9 +4,39 @@ import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
 public final class SdConstants {
-    public final static String UNDELETED = "N"; // 삭제되지 않은 데이터
-    public final static String INACTIVE = "N"; // 비활성화
-    public final static String STARTED = "Y"; // 시작
-    public final static String STOPPED = "N"; // 중지
-    public final static String COMPLETION = "N"; // 완료
+    /**
+     * deletedYn(삭제됨)
+     */
+    public final static String DELETED = "Y";
+    /**
+     * deletedYn(삭제안됨)
+     */
+    public final static String UNDELETED = "N";
+
+    /**
+     * dpSt(배포중)
+     */
+    public final static String ACTIVE = "Y";
+
+    /**
+     * dpSt(배포중지)
+     */
+    public final static String INACTIVE = "N";
+
+    /**
+     * startedYn(시작)
+     */
+    public final static String STARTED = "Y";
+    /**
+     * startedYn(중지)
+     */
+    public final static String STOPPED = "N";
+    /**
+     * rcsSt(수집중)
+     */
+    public final static String COLLECTING = "Y";
+    /**
+     * rcsSt(완료)
+     */
+    public final static String COMPLETION = "N";
 }
--- src/main/java/com/ks/sd/schd/SvnHistoryLoaderTask.java
+++ src/main/java/com/ks/sd/schd/SvnHistoryLoaderTask.java
@@ -3,6 +3,7 @@ package com.ks.sd.schd;
 import java.util.function.BiConsumer;
 
 import com.ks.sd.api.pjt.service.ProjectService;
+import com.ks.sd.consts.SdConstants;
 
 import lombok.RequiredArgsConstructor;
 
@@ -13,19 +14,16 @@ public class SvnHistoryLoaderTask implements Runnable {
     private final SvnHistoryLoaderService svnHistoryLoaderService;
     private final ProjectService projectService;
 
-    final String COMMENCEMENT = "Y";
-    final String COMPLETION = "N";
-
     @Override
     public void run() {
         try {
             // 리비전 수집 시작 상태 업데이트
-            projectService.updateProjectByRcsSt(pjtNo, COMMENCEMENT);
+            projectService.updateProjectByRcsSt(pjtNo, SdConstants.COLLECTING);
             // 리비전 수집
             svnHistoryLoaderService.run(pjtNo);
         } finally {
             // 리비전 수집 완료 상태 업데이트
-            projectService.updateProjectByRcsSt(pjtNo, COMPLETION);
+            projectService.updateProjectByRcsSt(pjtNo, SdConstants.COMPLETION);
             // 스레드 상태 업데이트
             threadStatusUpdater.accept(pjtNo, true);
         }
