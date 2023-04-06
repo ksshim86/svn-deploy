147cff5664149a341c43f325d9e4977fae1e9837
ksshim86 <ksshim86@gmail.com>
2023-04-06 15:15:03 +0900

    # 소스 리팩토링

--- src/main/java/com/ks/sd/api/appr/controller/AppPrController.java
+++ src/main/java/com/ks/sd/api/appr/controller/AppPrController.java
@@ -3,7 +3,6 @@ package com.ks.sd.api.appr.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -12,30 +11,27 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ks.sd.api.appr.dto.AppPrUpdateRequest;
-import com.ks.sd.api.appr.dto.AppPrView;
+import com.ks.sd.api.appr.dto.AppPrResponse;
 import com.ks.sd.api.appr.service.AppPrService;
 
 @RestController
-@RequestMapping("/api/app-pr")
+@RequestMapping("/api/projects/{pjtNo}/roles/app-prs")
 public class AppPrController {
     @Autowired
     private AppPrService appPrService;
 
-    @GetMapping("/pjt-no/{pjtNo}")
-    public ResponseEntity<List<AppPrView>> getAppPrsByPjtNo(@PathVariable Integer pjtNo) {
-        List<AppPrView> appPrViews = appPrService.getAppPrsByPjtNo(pjtNo);
-
-        return ResponseEntity.ok(appPrViews);
+    @GetMapping
+    public List<AppPrResponse> getAppPrsByPjtNo(@PathVariable Integer pjtNo) {
+        return appPrService.getAppPrsByPjtNo(pjtNo);
     }
 
-    @PutMapping("/pjt-no/{pjtNo}")
-    public ResponseEntity<Void> updateAppPrByPjtNo(
+    @PutMapping
+    public AppPrResponse updateUseYnByPjtNoAndOrdr(
         @PathVariable Integer pjtNo,
         @RequestBody AppPrUpdateRequest appPrUpdateRequest
     ) {
         appPrUpdateRequest.setPjtNo(pjtNo);
-        appPrService.updateAppPrByPjtNo(appPrUpdateRequest);
 
-        return ResponseEntity.noContent().build();
+        return appPrService.updateUseYnByPjtNoAndOrdr(appPrUpdateRequest);
     }
 }
--- src/main/java/com/ks/sd/api/appr/service/AppPrService.java
+++ src/main/java/com/ks/sd/api/appr/service/AppPrService.java
@@ -9,8 +9,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.ks.sd.api.appr.dto.AppPrUpdateRequest;
-import com.ks.sd.api.appr.dto.AppPrView;
+import com.ks.sd.api.appr.dto.AppPrResponse;
 import com.ks.sd.api.appr.entity.AppPr;
+import com.ks.sd.api.appr.entity.AppPr.AppPrId;
 import com.ks.sd.api.appr.repository.AppPrRepository;
 import com.ks.sd.api.pjt.entity.Project;
 import com.ks.sd.api.role.dto.RoleView;
@@ -28,53 +29,71 @@ public class AppPrService {
     @Autowired
     private RoleService roleService;
     
-    public List<AppPrView> getAppPrsByPjtNo(Integer pjtNo) {
+    /**
+     * 주어진 프로젝트 번호에 해당하는 승인 절차를 반환합니다.
+     * @param pjtNo 승인 절차를 반환할 프로젝트 번호입니다.
+     * @return 승인 절차를 나타내는 AppPrResponse 인스턴스의 목록을 반환합니다.
+     */
+    public List<AppPrResponse> getAppPrsByPjtNo(Integer pjtNo) {
         Project project = Project.builder().pjtNo(pjtNo).build();
 
-        List<AppPr> appPrs = 
-            appPrRepository.findByProject(project).orElseThrow(() -> new BusinessException(ErrorCode.APPRS_NOT_FOUND));
-        List<AppPrView> appPrViews = 
-            appPrs.stream()
-                .map(appPr -> AppPrView.builder().appPr(appPr).build())
-                .collect(Collectors.toList());
-
-        return appPrViews;
-    }
-
-    public List<AppPrView> saveAppPrs(Project project) {
-        List<RoleView> roleViews = roleService.getAllRoles();
-
-        List<AppPr> appPrs = IntStream.range(0, roleViews.size())
-            .mapToObj(i -> {
-                RoleView roleView = roleViews.get(i);
-                return AppPr.builder()
-                    .project(project)
-                    .role(Role.builder().roleCd(roleView.getRoleCd()).build())
-                    .ordr(i + 1)
-                    .build();
-            })
-            .collect(Collectors.toList());
-
-        List<AppPr> saveAppPrs = appPrRepository.saveAll(appPrs);
-        List<AppPrView> appPrViews = 
-            saveAppPrs.stream()
-                .map(appPr -> AppPrView.builder().appPr(appPr).build())
-                .collect(Collectors.toList());
-
-        return appPrViews;
-    }
-    
-    // 승인 절차 사용여부 변경
-    public void updateAppPrByPjtNo(AppPrUpdateRequest appPrUpdateRequest) {
-        Project project = Project.builder().pjtNo(appPrUpdateRequest.getPjtNo()).build();
         List<AppPr> appPrs = appPrRepository.findByProject(project)
             .orElseThrow(() -> new BusinessException(ErrorCode.APPRS_NOT_FOUND));
-        
-        appPrs.stream()
-            .filter(appPr -> appPr.getOrdr().equals(appPrUpdateRequest.getOrdr()))
-            .findFirst()
-            .ifPresent(appPr -> {
-                appPr.updateUseYn(appPrUpdateRequest.getUseYn());
-            });
+
+        List<AppPrResponse> responses = appPrs.stream()
+                .map(appPr -> AppPrResponse.builder().appPr(appPr).build())
+                .collect(Collectors.toList());
+
+        return responses;
+    }
+
+    /**
+     * 주어진 프로젝트와 관련된 승인 절차를 저장합니다.
+     * @param project 승인 절차가 저장되는 프로젝트입니다.
+     * @return 저장된 승인 절차를 나타내는 AppPrResponse 인스턴스의 목록을 반환합니다.
+     */
+    public List<AppPrResponse> saveAppPrs(Project project) {
+        List<RoleView> roleViews = roleService.getAllRoles();
+
+        List<AppPr> appPrs = 
+            IntStream.range(0, roleViews.size())
+                .mapToObj(i -> {
+                    RoleView roleView = roleViews.get(i);
+                    return AppPr.builder()
+                        .project(project)
+                        .role(Role.builder().roleCd(roleView.getRoleCd()).build())
+                        .ordr(i + 1)
+                        .build();
+                })
+                .collect(Collectors.toList());
+
+        List<AppPr> saveAppPrs = appPrRepository.saveAll(appPrs);
+
+        List<AppPrResponse> responses = 
+            saveAppPrs.stream()
+                .map(appPr -> AppPrResponse.builder().appPr(appPr).build())
+                .collect(Collectors.toList());
+
+        return responses;
+    }
+    
+    /**
+     * 주어진 프로젝트 번호와 순서에 해당하는 승인 절차의 사용 여부를 수정합니다.
+     * @param appPrUpdateRequest 승인 절차의 사용 여부를 수정하는 요청입니다.
+     * @return 수정된 승인 절차를 나타내는 AppPrResponse 인스턴스를 반환합니다.
+     */
+    public AppPrResponse updateUseYnByPjtNoAndOrdr(AppPrUpdateRequest appPrUpdateRequest) {
+        AppPrId appPrId = 
+            AppPrId.builder()
+                .project(appPrUpdateRequest.getPjtNo())
+                .ordr(appPrUpdateRequest.getOrdr())
+                .build();
+
+        AppPr appPr = appPrRepository.findById(appPrId)
+                .orElseThrow(() -> new BusinessException(ErrorCode.APPRS_NOT_FOUND));
+
+        appPr.updateUseYn(appPrUpdateRequest.getUseYn());
+
+        return AppPrResponse.builder().appPr(appPr).build();
     }
 }
